2ba84d6f9ac45b5e3e245f0a343bd28417d2add3
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2023-02-10 00:08:37 -0300

    classe main

---
+++ SistemaAcademia/src/Main.java
@@ -0,0 +1,10 @@
+import br.edu.ifpe.edu.paulista.tadala_fit.ui.FrameLogin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FrameLogin fl = new FrameLogin();
+		fl.framelogin.setVisible(true);
+	}
+
+}
